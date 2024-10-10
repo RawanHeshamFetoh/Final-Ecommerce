7f7ca6626ad67a68c19259cfaa2ab57054b9d9f5
ahmed <ah9486052@gmail.com>
2024-10-10 20:41:17 +0300

    first push for the final Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProductPage from './pages/ProductPage/index.jsx';
+import ProductCategory from './pages/ProductCategory/index.jsx';
+import CartPage from './pages/Cart/index.jsx';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Router>
+                <Header />
+                <div className="flex-grow-1">
+                    {/* Main content area */}
+                    <Routes>
+                        <Route path="/ProductPage" element={<ProductPage />} />
+                        <Route path="/ProductCategory" element={<ProductCategory />} />
+                        <Route path="/CartPage" element={<CartPage />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
